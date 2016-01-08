647db05d994dda79dd82ee972b9f9744e70c69b5
foxx1337 <foxx1337@yahoo.com>
2016-01-08 03:02:02 +0100

    initial commit

---
+++ TestExceptionRaii/MyClass.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+	void DoNasties() const;
+
+private:
+	void Foobar() const;
+	int unused;
+};
+
+
+
+MyClass::MyClass()
+{
+	cout << "constructing" << endl;
+}
+
+MyClass::~MyClass()
+{
+	cout << "destructing" << endl;
+}
+
+void MyClass::DoNasties() const
+{
+	Foobar();
+	throw exception("generic exception thrown");
+}
+
+void MyClass::Foobar() const
+{
+	cout << "cheating..." << endl;
+	cout << "sizeof(MyClass) = " << sizeof(*this) << endl;
+}
---
+++ TestExceptionRaii/main.cpp
@@ -0,0 +1,19 @@
+#include "MyClass.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try
+	{
+		MyClass a;
+		cout << "sizeof(MyClass) = " << sizeof(a) << endl;
+		a.DoNasties();
+	}
+	catch (exception &e)
+	{
+		cout << "should see " << e.what() << ", but after the destructor" << endl;
+	}
+	return 0;
+}
